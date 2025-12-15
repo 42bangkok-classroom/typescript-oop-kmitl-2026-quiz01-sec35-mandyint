8d7ff0787e92f6ddc09a02ee7ec93d77000929a5
Pimlada Patiphatpatawee <pxcopic@gmail.com>
2025-12-15 10:05:12 +0700

    nadia

--- src/q03/calc.ts
+++ src/q03/calc.ts
@@ -1,28 +1,28 @@
 const op = process.argv[2];
-const num1 = +process.argv[3];
-const num2 = +process.argv[4];
+const num1 = process.argv[3];
+const num2 = process.argv[4];
 
-if(op == null || Number.isNaN(num1) || Number.isNaN(num2)){
-    console.log("Invalid Input");
+if(op == null || Number.isNaN(num1) || Number.isNaN(num2) || num1 === "" || num2 === ""){
+    console.log("Invalid input");
 }else{
     switch(op){
         case 'add':
-            console.log(num1 + num2);
+            console.log((+num1 + +num2).toString);
             break;
     
         case 'sub':
-            console.log(num1 - num2);
+            console.log((+num1 - +num2).toString);
             break;
         
         case 'mul':
-            console.log(num1 * num2);
+            console.log((+num1 * +num2).toString);
             break;
     
         case 'div':
-            if(num1 === 0 || num2 === 0){
+            if(+num1 === 0 || +num2 === 0){
                 console.log("Invalid input");
             }else{
-                console.log(num1 / num2);
+                console.log((+num1 / +num2).toString);
             }
             break;
     
