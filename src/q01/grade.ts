e07962468709eaac66075d0b0bc5318e55656605
Pimlada Patiphatpatawee <pxcopic@gmail.com>
2025-12-15 10:47:18 +0700

    go

--- src/q01/grade.ts
+++ src/q01/grade.ts
@@ -1,14 +1,14 @@
-const grade = process.argv[2];
-if(Number.isNaN(grade) || +grade < 0 || +grade > 100 || grade === ""){
+const grade = +process.argv[2];
+if(Number.isNaN(grade) || +grade < 0 || +grade > 100 || process.argv[2].trim() === ""){
     console.log("Invalid Input");
-}else if(+grade >= 80){
+}else if(grade >= 80){
     console.log("Grade is A");
-}else if(+grade >= 70){
+}else if(grade >= 70){
     console.log("Grade is B");
-}else if(+grade >= 60){
+}else if(grade >= 60){
     console.log("Grade is C");
-}else if(+grade >= 50){
+}else if(grade >= 50){
     console.log("Grade is D");
-}else if(+grade < 50){
+}else if(grade < 50){
     console.log("Grade is F");
 }
