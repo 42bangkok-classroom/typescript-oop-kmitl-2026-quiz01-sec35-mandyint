31ef4181fc42ac2b7d37004f1ea1af5f13e1a506
Pimlada Patiphatpatawee <pxcopic@gmail.com>
2025-12-15 10:09:50 +0700

    efeji

--- src/q01/grade.ts
+++ src/q01/grade.ts
@@ -1,13 +1,13 @@
-const grade = +process.argv[2];
-if(Number.isNaN(grade) || grade < 0){
+const grade = process.argv[2];
+if(Number.isNaN(grade) || +grade < 0 || +grade > 100 || grade === ""){
     console.log("Invalid Input");
-}else if(grade >= 80){
+}else if(+grade >= 80){
     console.log("Grade is A");
-}else if(grade >= 70){
+}else if(+grade >= 70){
     console.log("Grade is B");
-}else if(grade >= 60){
+}else if(+grade >= 60){
     console.log("Grade is C");
-}else if(grade >= 50){
+}else if(+grade >= 50){
     console.log("Grade is D");
 }else{
     console.log("Grade is F");
